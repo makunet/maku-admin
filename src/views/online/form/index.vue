2960ba376632ea323aca82626c98071c2f651dac
阿沐 <babamu@126.com>
2023-03-22 00:13:16 +0800

    在线开发

---
+++ src/views/online/form/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<el-card>
+		<online-form :route="route" :dict-list="dictList"></online-form>
+	</el-card>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import store from '@/store'
+
+const route = useRoute()
+const dictList = store.appStore.dictList
+</script>
---
+++ src/views/online/table/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<el-card>
+		<online-table :router="router"></online-table>
+	</el-card>
+</template>
+
+<script setup lang="ts" name="OnlineTableIndex">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
