301a3b8c973f2aa9e208b2075a97e72d254cb745
阿沐 <babamu@126.com>
2023-03-22 00:15:19 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,44 @@ import { directive } from './utils/directive'
 import { router } from './router'
 import { i18n } from './i18n'
 import 'virtual:svg-icons-register'
+import MakuFormDesign from 'maku-form-design'
+import 'maku-form-design/lib/style.css'
 
 import '@/icons/iconfont/iconfont'
 import 'element-plus/dist/index.css'
 import '@/styles/index.scss'
+
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
 
 import FastTableColumn from '@/components/fast-table-column'
 import FastRadioGroup from '@/components/fast-radio-group'
 import FastSelect from '@/components/fast-select'
 import FastUser from '@/components/fast-user'
 import SvgIcon from '@/components/svg-icon'
-const app = createApp(App)
 
+import { registerOnline } from './utils/online'
+
+VXETable.setup({
+	zIndex: 3000,
+	select: {
+		transfer: true
+	}
+})
+
+const app = createApp(App)
 app.use(createPinia())
+
 // 注册 Pinia
 registerStore()
+
+// 表单设计器
+app.use(MakuFormDesign)
+
+// 注册 在线开发
+registerOnline(app)
+
 // 注册 自定义指令
 directive(app)
 app.use(router)
@@ -32,4 +55,5 @@ app.use(FastSelect)
 app.use(FastUser)
 app.use(SvgIcon)
 app.use(ElementPlus)
+app.use(VXETable)
 app.mount('#app')
