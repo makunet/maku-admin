78bfaca8c800541285a46a32fcd4e632b9486168
阿沐 <babamu@126.com>
2023-10-05 23:50:07 +0800

    新增markdown编辑器demo

---
+++ src/views/demo/mdeditor/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<MdEditor v-model="editorValue" height="500px"></MdEditor>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import MdEditor from '@/components/md-editor/index.vue'
+import { ref } from 'vue'
+
+const editorValue = ref('maku')
+</script>
