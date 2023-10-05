c9bd16ec0d632e9dd4ca5ec03eaff61c195ce9c8
阿沐 <babamu@126.com>
2023-10-05 23:48:50 +0800

    新增 markdown 编辑器组件

---
+++ src/components/md-editor/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<v-md-editor :model-value="modelValue" :height="height" @change="handleChange"></v-md-editor>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import VMdEditor from '@kangc/v-md-editor'
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import '@kangc/v-md-editor/lib/theme/style/github.css'
+
+// highlight
+import hljs from 'highlight.js'
+VMdEditor.use(githubTheme, {
+	Hljs: hljs
+})
+
+const props = defineProps({
+	modelValue: {
+		type: String,
+		required: true
+	},
+	height: {
+		type: String,
+		default: '400px'
+	}
+})
+
+// 编辑器change事件触发
+const emit = defineEmits(['update:modelValue'])
+const handleChange = (text: string, html: string) => {
+	emit('update:modelValue', text)
+}
+</script>
