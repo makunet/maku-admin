a239ee888c71a1590500ab43938db2843aef7079
阿沐 <babamu@126.com>
2024-02-24 15:55:43 +0800

    新增第三方配置

---
+++ src/views/sys/third/config/add-or-update.vue
@@ -0,0 +1,106 @@
+<template>
+	<el-dialog v-model="visible" :title="!dataForm.id ? '新增' : '修改'" :close-on-click-modal="false">
+		<el-form ref="dataFormRef" :model="dataForm" :rules="dataRules" label-width="100px" @keyup.enter="submitHandle()">
+			<el-form-item label="平台类型" prop="openType">
+				<el-select v-model="dataForm.openType" placeholder="请选择" style="width: 100%">
+					<el-option label="企业微信" value="wechat_work"></el-option>
+					<el-option label="钉钉" value="dingtalk"></el-option>
+					<el-option label="飞书" value="feishu"></el-option>
+					<el-option label="微信" value="wechat_open"></el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item label="ClientID" prop="clientId">
+				<el-input v-model="dataForm.clientId" placeholder="ClientID"></el-input>
+			</el-form-item>
+			<el-form-item label="ClientSecret" prop="clientSecret">
+				<el-input v-model="dataForm.clientSecret" placeholder="ClientSecret"></el-input>
+			</el-form-item>
+			<el-form-item label="RedirectUri" prop="redirectUri">
+				<el-input v-model="dataForm.redirectUri" placeholder="RedirectUri"></el-input>
+			</el-form-item>
+			<el-form-item label="AgentID" prop="agentId" v-if="dataForm.openType === 'wechat_work'">
+				<el-input v-model="dataForm.agentId" placeholder="AgentID"></el-input>
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<el-button @click="visible = false">取消</el-button>
+			<el-button type="primary" @click="submitHandle()">确定</el-button>
+		</template>
+	</el-dialog>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import { ElMessage } from 'element-plus/es'
+import { useThirdConfigApi, useThirdConfigSubmitApi } from '@/api/sys/third'
+
+const emit = defineEmits(['refreshDataList'])
+
+const visible = ref(false)
+const dataFormRef = ref()
+
+const dataForm = reactive({
+	id: '',
+	openType: '',
+	clientId: '',
+	clientSecret: '',
+	redirectUri: '',
+	agentId: '',
+	tenantId: '',
+	version: '',
+	deleted: '',
+	createTime: ''
+})
+
+const init = (id?: number) => {
+	visible.value = true
+	dataForm.id = ''
+
+	// 重置表单数据
+	if (dataFormRef.value) {
+		dataFormRef.value.resetFields()
+	}
+
+	if (id) {
+		getSys_third_login_config(id)
+	}
+}
+
+const getSys_third_login_config = (id: number) => {
+	useThirdConfigApi(id).then(res => {
+		Object.assign(dataForm, res.data)
+	})
+}
+
+const dataRules = ref({
+	openType: [{ required: true, message: '必填项不能为空', trigger: 'blur' }],
+	clientId: [{ required: true, message: '必填项不能为空', trigger: 'blur' }],
+	clientSecret: [{ required: true, message: '必填项不能为空', trigger: 'blur' }],
+	redirectUri: [{ required: true, message: '必填项不能为空', trigger: 'blur' }],
+	agentId: [{ required: true, message: '必填项不能为空', trigger: 'blur' }]
+})
+
+// 表单提交
+const submitHandle = () => {
+	dataFormRef.value.validate((valid: boolean) => {
+		if (!valid) {
+			return false
+		}
+
+		useThirdConfigSubmitApi(dataForm).then(() => {
+			ElMessage.success({
+				message: '操作成功',
+				duration: 500,
+				onClose: () => {
+					visible.value = false
+					emit('refreshDataList')
+				}
+			})
+		})
+	})
+}
+
+defineExpose({
+	init
+})
+</script>
---
+++ src/views/sys/third/config/index.vue
@@ -0,0 +1,71 @@
+<template>
+	<el-card>
+		<el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
+			<el-form-item>
+				<el-button type="primary" @click="addOrUpdateHandle()">新增</el-button>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="danger" @click="deleteBatchHandle()">删除</el-button>
+			</el-form-item>
+		</el-form>
+		<el-table
+			v-loading="state.dataListLoading"
+			:data="state.dataList"
+			show-overflow-tooltip
+			border
+			style="width: 100%"
+			@selection-change="selectionChangeHandle"
+		>
+			<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+			<el-table-column prop="openType" label="平台类型" header-align="center" align="center" width="180">
+				<template #default="scope">
+					<el-text v-if="scope.row.openType === 'wechat_work'"><svg-icon icon="icon-workweixin" size="16" /> 企业微信</el-text>
+					<el-text v-else-if="scope.row.openType === 'dingtalk'"><svg-icon icon="icon-dingding" size="16" /> 钉钉</el-text>
+					<el-text v-else-if="scope.row.openType === 'feishu'"><svg-icon icon="icon-feishu" size="16" /> 飞书</el-text>
+					<el-text v-else-if="scope.row.openType === 'wechat_open'"><svg-icon icon="icon-weixin" size="16" /> 微信</el-text>
+				</template>
+			</el-table-column>
+			<el-table-column prop="clientId" label="ClientID" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="clientSecret" label="ClientSecret" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="redirectUri" label="RedirectUri" header-align="center" align="center"></el-table-column>
+			<el-table-column label="操作" fixed="right" header-align="center" align="center" width="120">
+				<template #default="scope">
+					<el-button type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+					<el-button type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+		<el-pagination
+			:current-page="state.page"
+			:page-sizes="state.pageSizes"
+			:page-size="state.limit"
+			:total="state.total"
+			layout="total, sizes, prev, pager, next, jumper"
+			@size-change="sizeChangeHandle"
+			@current-change="currentChangeHandle"
+		>
+		</el-pagination>
+
+		<!-- 弹窗, 新增 / 修改 -->
+		<add-or-update ref="addOrUpdateRef" @refreshDataList="getDataList"></add-or-update>
+	</el-card>
+</template>
+
+<script setup lang="ts">
+import { useCrud } from '@/hooks'
+import { reactive, ref } from 'vue'
+import { IHooksOptions } from '@/hooks/interface'
+import AddOrUpdate from './add-or-update.vue'
+
+const state: IHooksOptions = reactive({
+	dataListUrl: '/sys/third/config/page',
+	deleteUrl: '/sys/third/config'
+})
+
+const addOrUpdateRef = ref()
+const addOrUpdateHandle = (id?: number) => {
+	addOrUpdateRef.value.init(id)
+}
+
+const { getDataList, selectionChangeHandle, sizeChangeHandle, currentChangeHandle, deleteBatchHandle } = useCrud(state)
+</script>
