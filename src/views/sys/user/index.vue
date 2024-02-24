2ce638f2374d2a359e3e08c77e7df7624f8c0005
阿沐 <babamu@126.com>
2024-02-24 15:51:41 +0800

    用户管理，新增机构过滤

--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -1,71 +1,79 @@
 <template>
-	<el-card>
-		<el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
-			<el-form-item>
-				<el-input v-model="state.queryForm.username" placeholder="用户名" clearable></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-input v-model="state.queryForm.mobile" placeholder="手机号" clearable></el-input>
-			</el-form-item>
-			<el-form-item>
-				<fast-select v-model="state.queryForm.gender" dict-type="user_gender" clearable placeholder="性别"></fast-select>
-			</el-form-item>
-			<el-form-item>
-				<el-button @click="getDataList()">查询</el-button>
-			</el-form-item>
-			<el-form-item>
-				<el-button v-auth="'sys:user:save'" type="primary" @click="addOrUpdateHandle()">新增</el-button>
-			</el-form-item>
-			<el-form-item>
-				<el-button v-auth="'sys:user:delete'" type="danger" @click="deleteBatchHandle()">删除</el-button>
-			</el-form-item>
-			<el-form-item v-auth="'sys:user:import'">
-				<el-upload :action="uploadUserExcelUrl" :before-upload="beforeUpload" :on-success="handleSuccess" :show-file-list="false">
-					<el-button type="info">导入</el-button>
-				</el-upload>
-			</el-form-item>
-			<el-form-item>
-				<el-button v-auth="'sys:user:export'" type="success" @click="downloadExcel()">导出</el-button>
-			</el-form-item>
-		</el-form>
-		<el-table
-			v-loading="state.dataListLoading"
-			show-overflow-tooltip
-			:data="state.dataList"
-			border
-			style="width: 100%"
-			@selection-change="selectionChangeHandle"
-		>
-			<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-			<el-table-column prop="username" label="用户名" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="realName" label="姓名" header-align="center" align="center"></el-table-column>
-			<fast-table-column prop="gender" label="性别" dict-type="user_gender"></fast-table-column>
-			<el-table-column prop="mobile" label="手机号" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="email" label="邮箱" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="orgName" label="所属机构" header-align="center" align="center"></el-table-column>
-			<fast-table-column prop="status" label="状态" dict-type="user_status"></fast-table-column>
-			<el-table-column prop="createTime" label="创建时间" header-align="center" align="center" width="170"></el-table-column>
-			<el-table-column label="操作" fixed="right" header-align="center" align="center" width="150">
-				<template #default="scope">
-					<el-button v-auth="'sys:user:update'" type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
-					<el-button v-auth="'sys:user:delete'" type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
-				</template>
-			</el-table-column>
-		</el-table>
-		<el-pagination
-			:current-page="state.page"
-			:page-sizes="state.pageSizes"
-			:page-size="state.limit"
-			:total="state.total"
-			layout="total, sizes, prev, pager, next, jumper"
-			@size-change="sizeChangeHandle"
-			@current-change="currentChangeHandle"
-		>
-		</el-pagination>
+	<el-row :gutter="6">
+		<el-col :span="5">
+			<el-card>
+				<OrgTree @node-click="handleOrgClick" />
+			</el-card>
+		</el-col>
+		<el-col :span="19">
+			<el-card>
+				<el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
+					<el-form-item>
+						<el-input v-model="state.queryForm.username" placeholder="用户名" clearable></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-input v-model="state.queryForm.mobile" placeholder="手机号" clearable></el-input>
+					</el-form-item>
+					<!--					<el-form-item>-->
+					<!--						<fast-select v-model="state.queryForm.gender" dict-type="user_gender" clearable placeholder="性别"></fast-select>-->
+					<!--					</el-form-item>-->
+					<el-form-item>
+						<el-button @click="getDataList()">查询</el-button>
+					</el-form-item>
+					<el-form-item>
+						<el-button v-auth="'sys:user:save'" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+					</el-form-item>
+					<el-form-item>
+						<el-button v-auth="'sys:user:delete'" type="danger" @click="deleteBatchHandle()">删除</el-button>
+					</el-form-item>
+					<el-form-item v-auth="'sys:user:import'">
+						<el-upload :action="uploadUserExcelUrl" :before-upload="beforeUpload" :on-success="handleSuccess" :show-file-list="false">
+							<el-button type="info">导入</el-button>
+						</el-upload>
+					</el-form-item>
+					<el-form-item>
+						<el-button v-auth="'sys:user:export'" type="success" @click="downloadExcel()">导出</el-button>
+					</el-form-item>
+				</el-form>
+				<el-table
+					v-loading="state.dataListLoading"
+					show-overflow-tooltip
+					:data="state.dataList"
+					border
+					style="width: 100%"
+					@selection-change="selectionChangeHandle"
+				>
+					<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+					<el-table-column prop="username" label="用户名" header-align="center" align="center"></el-table-column>
+					<el-table-column prop="realName" label="姓名" header-align="center" align="center"></el-table-column>
+					<fast-table-column prop="gender" label="性别" dict-type="user_gender"></fast-table-column>
+					<el-table-column prop="mobile" label="手机号" header-align="center" align="center"></el-table-column>
+					<el-table-column prop="orgName" label="所属机构" header-align="center" align="center"></el-table-column>
+					<fast-table-column prop="status" label="状态" dict-type="user_status"></fast-table-column>
+					<el-table-column prop="createTime" label="创建时间" header-align="center" align="center"></el-table-column>
+					<el-table-column label="操作" fixed="right" header-align="center" align="center" width="120">
+						<template #default="scope">
+							<el-button v-auth="'sys:user:update'" type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+							<el-button v-auth="'sys:user:delete'" type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
+						</template>
+					</el-table-column>
+				</el-table>
+				<el-pagination
+					:current-page="state.page"
+					:page-sizes="state.pageSizes"
+					:page-size="state.limit"
+					:total="state.total"
+					layout="total, sizes, prev, pager, next, jumper"
+					@size-change="sizeChangeHandle"
+					@current-change="currentChangeHandle"
+				>
+				</el-pagination>
+			</el-card>
+		</el-col>
+	</el-row>
 
-		<!-- 弹窗, 新增 / 修改 -->
-		<add-or-update ref="addOrUpdateRef" @refresh-data-list="getDataList"></add-or-update>
-	</el-card>
+	<!-- 弹窗, 新增 / 修改 -->
+	<add-or-update ref="addOrUpdateRef" @refresh-data-list="getDataList"></add-or-update>
 </template>
 
 <script setup lang="ts" name="SysUserIndex">
@@ -74,6 +82,7 @@ import { reactive, ref } from 'vue'
 import AddOrUpdate from './add-or-update.vue'
 import { IHooksOptions } from '@/hooks/interface'
 import { ElMessage, UploadProps } from 'element-plus'
+import OrgTree from './org-tree.vue'
 import cache from '@/utils/cache'
 import constant from '@/utils/constant'
 
@@ -83,13 +92,19 @@ const state: IHooksOptions = reactive({
 	queryForm: {
 		username: '',
 		mobile: '',
-		gender: ''
+		gender: '',
+		orgId: ''
 	}
 })
 
 const addOrUpdateRef = ref()
 const addOrUpdateHandle = (id?: number) => {
 	addOrUpdateRef.value.init(id)
+}
+
+const handleOrgClick = (orgId: number) => {
+	state.queryForm.orgId = orgId
+	getDataList()
 }
 
 // 导入用户excel文件
---
+++ src/views/sys/user/org-tree.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="org-container">
+		<el-input v-model="orgName" clearable placeholder="请输入关键字过滤" />
+		<el-tree
+			ref="treeRef"
+			:data="orgList"
+			:expand-on-click-node="false"
+			:filter-node-method="filterNode"
+			:props="{ label: 'name', children: 'children' }"
+			highlight-current
+			node-key="id"
+			@node-click="handleNodeClick"
+		/>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { ElTree } from 'element-plus'
+import { useOrgListApi } from '@/api/sys/orgs'
+import { onMounted, ref, watch } from 'vue'
+
+const orgList = ref()
+const orgName = ref()
+const treeRef = ref()
+
+onMounted(async () => {
+	await getOrgList()
+})
+
+watch(orgName, val => {
+	treeRef.value.filter(val)
+})
+
+// 机构列表
+const getOrgList = async () => {
+	const res = await useOrgListApi()
+	orgList.value = res.data
+}
+
+const filterNode = (name: string, data: any) => {
+	if (!name) {
+		return true
+	}
+	return data.name.includes(name)
+}
+
+const emits = defineEmits(['nodeClick'])
+// 处理点击事件
+const handleNodeClick = (row: any) => {
+	emits('nodeClick', row.id)
+}
+</script>
+
+<style lang="scss" scoped>
+.org-container {
+	min-height: calc(100vh - 112px - var(--theme-header-height));
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	:deep(.el-tree) {
+		margin-top: 20px;
+	}
+}
+</style>
