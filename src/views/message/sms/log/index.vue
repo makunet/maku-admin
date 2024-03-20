1f31fc4dfa34ae2f6aa851483588c45ed2b029f6
阿沐 <babamu@126.com>
2024-03-20 09:21:39 +0800

    布局调整

--- src/views/message/sms/log/index.vue
+++ src/views/message/sms/log/index.vue
@@ -1,17 +1,23 @@
 <template>
-	<el-card>
-		<el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
-			<el-form-item>
+	<el-card class="layout-query">
+		<el-form ref="queryRef" :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
+			<el-form-item prop="platformId">
 				<el-input v-model="state.queryForm.platformId" placeholder="平台ID"></el-input>
 			</el-form-item>
-			<el-form-item>
+			<el-form-item prop="platform">
 				<fast-select v-model="state.queryForm.platform" dict-type="sms_platform" clearable placeholder="平台类型"></fast-select>
 			</el-form-item>
 			<el-form-item>
-				<el-button @click="getDataList()">查询</el-button>
+				<el-button icon="Search" type="primary" @click="getDataList()">查询</el-button>
+			</el-form-item>
+			<el-form-item>
+				<el-button icon="RefreshRight" @click="reset(queryRef)">重置</el-button>
 			</el-form-item>
 		</el-form>
-		<el-table v-loading="state.dataListLoading" :data="state.dataList" border style="width: 100%" @selection-change="selectionChangeHandle">
+	</el-card>
+
+	<el-card>
+		<el-table v-loading="state.dataListLoading" :data="state.dataList" border class="layout-table" @selection-change="selectionChangeHandle">
 			<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
 			<el-table-column prop="platformId" label="平台ID" header-align="center" align="center"></el-table-column>
 			<fast-table-column prop="platform" label="平台类型" dict-type="sms_platform"></fast-table-column>
@@ -55,10 +61,11 @@ const state: IHooksOptions = reactive({
 	}
 })
 
+const queryRef = ref()
 const detailRef = ref()
 const detailHandle = (row: any) => {
 	detailRef.value.init(row)
 }
 
-const { getDataList, selectionChangeHandle, sizeChangeHandle, currentChangeHandle, deleteBatchHandle } = useCrud(state)
+const { getDataList, selectionChangeHandle, sizeChangeHandle, currentChangeHandle, deleteBatchHandle, reset } = useCrud(state)
 </script>
