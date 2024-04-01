2bede2b1614d053cbe6cca420eb43e8f8f233123
阿沐 <babamu@126.com>
2024-04-01 16:34:46 +0800

    新增生成uuid方法

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -27,6 +27,19 @@ export const convertSizeFormat = (size: number): string => {
 	return newSize.toFixed(2) + ' ' + unit[index]
 }
 
+// 生成uuid
+export const generateUUID = () => {
+	let uuid = ''
+	for (let i = 0; i < 32; i++) {
+		let random = (Math.random() * 16) | 0
+		if (i === 8 || i === 12 || i === 16 || i === 20) {
+			uuid += '-'
+		}
+		uuid += (i === 12 ? 4 : i === 16 ? (random & 3) | 8 : random).toString(16)
+	}
+	return uuid
+}
+
 // 获取svg图标(id)列表
 export const getIconList = (): string[] => {
 	const rs: string[] = []
