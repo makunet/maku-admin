e29388f11b95a7b03d8bb3dfbea86f497dbe3800
阿沐 <babamu@126.com>
2024-04-05 17:27:32 +0800

    字典单选框渲染组件

--- src/components/ma-dict/ma-dict-radio/index.vue
+++ src/components/ma-dict/ma-dict-radio/index.vue
@@ -1,24 +1,22 @@
 <template>
-	<el-radio-group :model-value="modelValue + ''" @change="$emit('update:modelValue', $event)">
+	<el-radio-group v-model="model">
 		<el-radio v-for="data in dataList" :key="data.dictValue" :value="data.dictValue">{{ data.dictLabel }}</el-radio>
 	</el-radio-group>
 </template>
 
-<script setup lang="ts" name="FastRadioGroup">
+<script setup lang="ts" name="MaDictRadio">
 import { useAppStore } from '@/store/modules/app'
 import { getDictDataList } from '@/utils/tool'
 
 const appStore = useAppStore()
 const props = defineProps({
-	modelValue: {
-		type: [Number, String],
-		required: true
-	},
 	dictType: {
 		type: String,
 		required: true
 	}
 })
+
+const model = defineModel<number | string>()
 
 const dataList = getDictDataList(appStore.dictList, props.dictType)
 </script>
