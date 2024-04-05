42008b53434ffc1a93fa43b7a2833d8871a08875
阿沐 <babamu@126.com>
2024-04-05 17:27:22 +0800

    字典文本渲染组件

---
+++ src/components/ma-dict/ma-dict-label/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<span v-html="getDictLabelList(dictType, dictValue)"></span>
+</template>
+
+<script setup lang="ts" name="MaDictLabel">
+import { getDictLabelList } from '@/utils/tool'
+
+defineProps({
+	dictType: {
+		type: String,
+		required: true
+	},
+	dictValue: {
+		type: [String, Number],
+		required: true
+	}
+})
+</script>
