71a6d4fee9034c8fee03d2f7c389d4d3c03243cd
阿沐 <babamu@126.com>
2024-04-05 17:32:35 +0800

    更新

--- src/views/sys/role/data-scope.vue
+++ src/views/sys/role/data-scope.vue
@@ -5,7 +5,7 @@
 				<el-input v-model="dataForm.name" disabled></el-input>
 			</el-form-item>
 			<el-form-item prop="dataScope" label="数据范围">
-				<fast-select v-model="dataForm.dataScope" dict-type="role_data_scope" placeholder="数据范围" style="width: 100%"></fast-select>
+				<ma-dict-select v-model="dataForm.dataScope" dict-type="role_data_scope" placeholder="数据范围" style="width: 100%"></ma-dict-select>
 			</el-form-item>
 			<el-form-item v-show="dataForm.dataScope == 4" label="数据权限">
 				<el-tree ref="orgListTree" :data="orgList" :props="{ label: 'name', children: 'children' }" node-key="id" accordion show-checkbox> </el-tree>
