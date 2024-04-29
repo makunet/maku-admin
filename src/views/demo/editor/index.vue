fc9eeec98daefd11796d4edd1607edc0a0461152
阿沐 <babamu@126.com>
2024-04-29 21:20:46 +0800

    更新

---
+++ src/views/demo/editor/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<ma-editor v-model="editorValue" placeholder="请输入..."></ma-editor>
+		<!--		<ma-editor v-model="editorValue2" :disabled="true" placeholder=""></ma-editor>-->
+	</div>
+</template>
+
+<script lang="ts" setup name="DemoEditorIndex">
+import { ref } from 'vue'
+
+const editorValue = ref('<p>maku</p>')
+// const editorValue2 = ref('<p>maku</p>')
+</script>
