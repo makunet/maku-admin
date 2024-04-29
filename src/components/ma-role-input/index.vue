0ba16aed67d2525a8dbd8bd166e1e8aa8a04a65e
阿沐 <babamu@126.com>
2024-04-29 21:17:01 +0800

    角色输入组件

---
+++ src/components/ma-role-input/index.vue
@@ -0,0 +1,78 @@
+<template>
+	<el-input v-model="roleName" :clearable="clearable" readonly :placeholder="placeholder" @clear="model = ''">
+		<template #append>
+			<el-button icon="Search" @click="visible = true"></el-button>
+		</template>
+	</el-input>
+	<ma-data-table
+		v-if="visible"
+		:key="visible + ''"
+		v-model:visible="visible"
+		title="角色选择"
+		url="/sys/role/page"
+		:multiple
+		:columns="[
+			{ field: 'name', label: '名称' },
+			{ field: 'roleCode', label: '编码' },
+			{ field: 'remark', label: '备注' }
+		]"
+		:query-params="[{ field: 'name', label: '名称' }]"
+		@select="onSelect"
+	></ma-data-table>
+</template>
+
+<script setup lang="ts" name="MaRoleInput">
+import { ref, watch } from 'vue'
+import { useRoleNameListApi } from '@/api/sys/role'
+
+const props = defineProps({
+	multiple: {
+		type: Boolean,
+		required: false,
+		default: false
+	},
+	clearable: {
+		type: Boolean,
+		required: false,
+		default: () => true
+	},
+	placeholder: {
+		type: String,
+		required: false,
+		default: () => ''
+	}
+})
+
+const model = defineModel<any | any[]>()
+const visible = ref(false)
+const roleName = ref()
+
+watch(
+	() => model.value,
+	async val => {
+		if (val) {
+			const idList = props.multiple ? val : [val]
+			if (idList.length > 0) {
+				const { data } = await useRoleNameListApi(idList)
+				roleName.value = data.join(',')
+			}
+		} else {
+			roleName.value = ''
+		}
+	},
+	{
+		immediate: true
+	}
+)
+
+const emit = defineEmits(['select'])
+const onSelect = (rows: any[]) => {
+	if (props.multiple) {
+		model.value = rows.map((item: any) => item.id)
+	} else {
+		model.value = rows[0].id
+	}
+
+	emit('select', rows)
+}
+</script>
