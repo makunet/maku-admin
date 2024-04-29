a496d8200c08bd7f975a5e2cdf6005104b4f0b3e
阿沐 <babamu@126.com>
2024-04-29 21:12:52 +0800

    新增用户名称列表接口

--- src/api/sys/user.ts
+++ src/api/sys/user.ts
@@ -16,8 +16,12 @@ export const updatePasswordApi = (data: any) => {
 	return service.put('/sys/user/password', data)
 }
 
-export const useUserApi = (id: number | string) => {
+export const useUserApi = (id: number) => {
 	return service.get('/sys/user/' + id)
+}
+
+export const useRealNameListApi = (idList: number[]) => {
+	return service.post('/sys/user/nameList', idList)
 }
 
 export const useUserSubmitApi = (dataForm: any) => {
