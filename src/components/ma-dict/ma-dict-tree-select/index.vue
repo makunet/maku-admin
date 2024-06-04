ea1bfbf7cb3c507ea912f9c1fedb902d30b0c074
阿沐 <babamu@126.com>
2024-06-04 11:04:45 +0800

    新增字典树选择组件

---
+++ src/components/ma-dict/ma-dict-tree-select/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<el-tree-select
+		v-model="modelAsString"
+		:data="dataList"
+		value-key="dictValue"
+		check-strictly
+		:render-after-expand="false"
+		:props="{
+			label: 'dictLabel',
+			children: 'children'
+		}"
+		style="width: 100%"
+		:clearable="clearable"
+		:placeholder="placeholder"
+		:disabled="disabled"
+	/>
+</template>
+
+<script setup lang="ts" name="MaDictTreeSelect">
+import { getDictDataList, treeDataTranslate } from '@/utils/tool'
+import { useAppStore } from '@/store/modules/app'
+import { computed } from 'vue'
+
+const appStore = useAppStore()
+const prop = defineProps({
+	dictType: {
+		type: String,
+		required: true
+	},
+	clearable: {
+		type: Boolean,
+		required: false,
+		default: () => false
+	},
+	disabled: {
+		type: Boolean,
+		required: false,
+		default: () => false
+	},
+	placeholder: {
+		type: String,
+		required: false,
+		default: () => ''
+	}
+})
+
+const model = defineModel<number | string>()
+const modelAsString = computed({
+	get() {
+		return model.value ? model.value.toString() : ''
+	},
+	set(value) {
+		model.value = value
+	}
+})
+
+const dictDataList = getDictDataList(appStore.dictList, prop.dictType)
+const dataList = treeDataTranslate(dictDataList, 'dictValue')
+</script>
