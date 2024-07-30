25a55e8b596b606b5bbc889da8e6c1c413056ec9
阿沐 <babamu@126.com>
2024-07-30 11:42:25 +0800

    移除日志接口

--- src/api/sys/log.ts
+++
@@ -1,7 +0,0 @@
-import cache from '@/utils/cache'
-import constant from '@/utils/constant'
-
-// 文件导出
-export const useLogLoginExportApi = () => {
-	location.href = constant.apiUrl + '/sys/log/login/export?access_token=' + cache.getToken()
-}
