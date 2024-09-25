af1b34a93416844651e5ccddc4dd3dc1f76ed4e6
阿沐 <babamu@126.com>
2024-09-25 14:16:24 +0800

    更新

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,13 +2,16 @@ import type { App, Plugin } from 'vue'
 import MaDictColumn from '@/components/ma-dict/ma-dict-column/index.vue'
 import MaDictLabel from '@/components/ma-dict/ma-dict-label/index.vue'
 import MaDictRadio from '@/components/ma-dict/ma-dict-radio/index.vue'
-import MaDictSelect from '@/components/ma-dict/ma-dict-select/index.vue'
 import MaDictTreeSelect from '@/components/ma-dict/ma-dict-tree-select/index.vue'
+import MaDictSelect from '@/components/ma-dict/ma-dict-select/index.vue'
+import MaDictCheckbox from '@/components/ma-dict/ma-dict-checkbox/index.vue'
 import MaDataColumn from '@/components/ma-data/ma-data-column/index.vue'
 import MaDataLabel from '@/components/ma-data/ma-data-label/index.vue'
 import MaDataSelect from '@/components/ma-data/ma-data-select/index.vue'
-import MaDataTable from '@/components/ma-data/ma-data-table/index.vue'
 import MaDataTreeSelect from '@/components/ma-data/ma-data-tree-select/index.vue'
+import MaDataTreeLeft from '@/components/ma-data/ma-data-tree-left/index.vue'
+import MaDataTable from '@/components/ma-data/ma-data-table/index.vue'
+import MaImageColumn from '@/components/ma-image-column/index.vue'
 
 import MaIcon from '@/components/ma-icon/index.vue'
 import MaEditor from '@/components/ma-editor/index.vue'
@@ -23,6 +26,7 @@ import MaPostInput from '@/components/ma-post-input/index.vue'
 import MaUploadImage from '@/components/ma-upload/ma-upload-image/index.vue'
 import MaUploadImages from '@/components/ma-upload/ma-upload-images/index.vue'
 import MaUploadFile from '@/components/ma-upload/ma-upload-file/index.vue'
+import MaUploadExcel from '@/components/ma-upload/ma-upload-excel/index.vue'
 
 const components = [
 	MaEditor,
@@ -33,12 +37,15 @@ const components = [
 	MaDictColumn,
 	MaDictRadio,
 	MaDictSelect,
+	MaDictCheckbox,
 	MaDictTreeSelect,
 	MaDataColumn,
 	MaDataLabel,
 	MaDataSelect,
 	MaDataTreeSelect,
+	MaDataTreeLeft,
 	MaDataTable,
+	MaImageColumn,
 	MaUserInput,
 	MaUserDialog,
 	MaOrgSelect,
@@ -47,7 +54,8 @@ const components = [
 	MaPostInput,
 	MaUploadImage,
 	MaUploadImages,
-	MaUploadFile
+	MaUploadFile,
+	MaUploadExcel
 ]
 
 const MaComponent: Plugin = {
