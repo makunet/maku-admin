b7306fcd07d6ac0e7beefc67ac59a783b6b96669
阿沐 <babamu@126.com>
2024-12-19 15:10:58 +0800

    登录成功后，跳转到之前页面

--- src/views/login/third.vue
+++ src/views/login/third.vue
@@ -14,6 +14,7 @@
 import constant from '@/utils/constant'
 import { useRouter } from 'vue-router'
 import { useUserStore } from '@/store/modules/user'
+import cache from '@/utils/cache'
 
 const router = useRouter()
 const userStore = useUserStore()
@@ -31,7 +32,7 @@ const thirdLogin = (openType: string) => {
 
 		// 第三方登录
 		userStore.thirdLoginAction(e.data).then(() => {
-			router.push({ path: constant.loginPage })
+			router.push({ path: cache.getRedirect() || constant.loginPage })
 		})
 	}
 }
