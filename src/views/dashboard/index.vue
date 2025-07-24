1ce2400d2bb234d1e4e353c4013db2e98081e611
阿沐 <babamu@126.com>
2025-07-24 22:04:59 +0800

    更新

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,23 +3,13 @@
 		<h2>介绍</h2>
 		<p>基于Vue3、TypeScript、Element Plus、Vue Router、Pinia、Axios、i18n、Vite等开发的后台管理</p>
 		<p>目前已实现maku-admin后端接口的有：【MakuBoot】和【MakuCloud】2个后端开源项目。</p>
+		<p>演示环境：<el-link href="https://demo.maku.net/maku-admin" type="primary" target="_blank"> https://demo.maku.net/maku-admin</el-link></p>
+		<p>开发文档：<el-link href="https://maku.net/docs/maku-admin" type="primary" target="_blank"> https://maku.net/docs/maku-admin</el-link></p>
 		<p>
-			演示环境：<el-link href="https://demo.maku.net/maku-admin" :underline="false" type="primary" target="_blank">
-				https://demo.maku.net/maku-admin</el-link
-			>
+			Gitee仓库：<el-link href="https://gitee.com/makunet/maku-admin" type="primary" target="_blank"> https://gitee.com/makunet/maku-admin</el-link>
 		</p>
 		<p>
-			开发文档：<el-link href="https://maku.net/docs/maku-admin" :underline="false" type="primary" target="_blank">
-				https://maku.net/docs/maku-admin</el-link
-			>
-		</p>
-		<p>
-			Gitee仓库：<el-link href="https://gitee.com/makunet/maku-admin" :underline="false" type="primary" target="_blank">
-				https://gitee.com/makunet/maku-admin</el-link
-			>
-		</p>
-		<p>
-			Github仓库：<el-link href="https://github.com/makunet/maku-admin" :underline="false" type="primary" target="_blank">
+			Github仓库：<el-link href="https://github.com/makunet/maku-admin" type="primary" target="_blank">
 				https://github.com/makunet/maku-admin</el-link
 			>
 		</p>
@@ -27,49 +17,27 @@
 
 		<h2>MakuBoot</h2>
 		<p>
-			Gitee仓库：<el-link href="https://gitee.com/makunet/maku-boot" :underline="false" type="primary" target="_blank">
-				https://gitee.com/makunet/maku-boot</el-link
-			>
+			Gitee仓库：<el-link href="https://gitee.com/makunet/maku-boot" type="primary" target="_blank"> https://gitee.com/makunet/maku-boot</el-link>
 		</p>
 		<p>
-			Github仓库：<el-link href="https://github.com/makunet/maku-boot" :underline="false" type="primary" target="_blank">
-				https://github.com/makunet/maku-boot</el-link
-			>
+			Github仓库：<el-link href="https://github.com/makunet/maku-boot" type="primary" target="_blank"> https://github.com/makunet/maku-boot</el-link>
 		</p>
 
-		<p>
-			演示环境：<el-link href="https://demo.maku.net/maku-boot" :underline="false" type="primary" target="_blank">
-				https://demo.maku.net/maku-boot</el-link
-			>
-		</p>
-		<p>
-			开发文档：<el-link href="https://maku.net/docs/maku-boot" :underline="false" type="primary" target="_blank">
-				https://maku.net/docs/maku-boot</el-link
-			>
-		</p>
+		<p>演示环境：<el-link href="https://demo.maku.net/maku-boot" type="primary" target="_blank"> https://demo.maku.net/maku-boot</el-link></p>
+		<p>开发文档：<el-link href="https://maku.net/docs/maku-boot" type="primary" target="_blank"> https://maku.net/docs/maku-boot</el-link></p>
 		<br />
 
 		<h2>MakuCloud</h2>
 		<p>
-			Gitee仓库：<el-link href="https://gitee.com/makunet/maku-cloud" :underline="false" type="primary" target="_blank">
-				https://gitee.com/makunet/maku-cloud</el-link
-			>
+			Gitee仓库：<el-link href="https://gitee.com/makunet/maku-cloud" type="primary" target="_blank"> https://gitee.com/makunet/maku-cloud</el-link>
 		</p>
 		<p>
-			Github仓库：<el-link href="https://github.com/makunet/maku-cloud" :underline="false" type="primary" target="_blank">
+			Github仓库：<el-link href="https://github.com/makunet/maku-cloud" type="primary" target="_blank">
 				https://github.com/makunet/maku-cloud</el-link
 			>
 		</p>
-		<p>
-			演示环境：<el-link href="https://demo.maku.net/maku-cloud" :underline="false" type="primary" target="_blank">
-				https://demo.maku.net/maku-cloud</el-link
-			>
-		</p>
-		<p>
-			开发文档：<el-link href="https://maku.net/docs/maku-cloud" :underline="false" type="primary" target="_blank">
-				https://maku.net/docs/maku-cloud</el-link
-			>
-		</p>
+		<p>演示环境：<el-link href="https://demo.maku.net/maku-cloud" type="primary" target="_blank"> https://demo.maku.net/maku-cloud</el-link></p>
+		<p>开发文档：<el-link href="https://maku.net/docs/maku-cloud" type="primary" target="_blank"> https://maku.net/docs/maku-cloud</el-link></p>
 		<br />
 
 		<h2>微信交流群</h2>
@@ -80,8 +48,8 @@
 		<h2>支持</h2>
 		<p>
 			如果觉得框架还不错，或者已经在使用了，希望你可以去
-			<el-link href="https://github.com/makunet" :underline="false" type="primary" target="_blank">Github</el-link> 或
-			<el-link href="https://gitee.com/makunet" :underline="false" type="primary" target="_blank">Gitee</el-link>
+			<el-link href="https://github.com/makunet" type="primary" target="_blank">Github</el-link> 或
+			<el-link href="https://gitee.com/makunet" type="primary" target="_blank">Gitee</el-link>
 			帮作者点个 ⭐ Star，这将是对作者极大的鼓励与支持。
 		</p>
 	</el-card>
